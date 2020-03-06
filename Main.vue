471b624a816b8594b67fd978a05309d4fd3c7ca6
lsr <roa234@gmail.com>
2020-03-06 13:51:50 +0100

    mobile responsiveness fixed

--- Main.vue
+++ Main.vue
@@ -29,7 +29,7 @@
                 .btn-container
                     button(style="border:2px #0c0c0c solid;background-color:#eee") Contact
                     p 
-                    button(style="background-color:#0c0c0c;color:#eee") Download CV
+                    a(download href="./assets/CV.pdf", style="background-color:#0c0c0c;color:#eee;border:none;display:inline-flex;justify-content:center;align-items:center;font-size:13.3") Download CV
                 .social
                     a.social-item(href="https://github.com/lsroa") 
                         <svg height="24" class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="24" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
@@ -656,7 +656,10 @@ button{
     .description{
         padding-top: 15px;
     }
-
+    #item-1{
+        grid-column: span 1;
+        grid-template-columns: 1fr;
+    }
     #item-3,#item-5 {
         grid-column: span 1;
     }
@@ -668,7 +671,9 @@ button{
         top: 4.2vh;
         /* fill: #aaa; */
     }
-
+    .gallery-container{
+        grid-template-columns: 1fr;
+    }
     .menu{
         justify-self: center;
     }
