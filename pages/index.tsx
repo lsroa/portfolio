204d134d747fb216078d4b8d872b9f2dbe734253
Luis Roa <roa234@gmail.com>
2022-12-05 09:35:30 +0100

    feat: 3d model

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,35 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { Canvas, ThreeEvent } from '@react-three/fiber'
+import {OrbitControls} from '@react-three/drei'
+import { Suspense, UIEventHandler,useCallback, useEffect, useState } from 'react'
 
 import { Card } from '@components/Card'
+import { Model } from '@components/Model'
+import { Vector3,Euler, Color } from 'three'
 
 export default function Home() {
+	const origin = new Vector3(0,0,0)
+	const [scrollY,setScrollY] = useState(0)
+
+	  const onScroll = useCallback( () => {
+      const { pageYOffset, scrollY } = window;
+
+      console.log("yOffset", pageYOffset, "scrollY", scrollY);
+      setScrollY(window.scrollY * -0.001);
+  }, []);
+
+  useEffect(() => {
+    //add eventlistener to window
+    window.addEventListener("scroll", onScroll);
+    // remove event on unmount to prevent a memory leak with the cleanup
+    return () => {
+       window.removeEventListener("scroll", onScroll);
+    }
+  }, [onScroll]);
+
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto h-[2000px]">
       <Head>
         <title>lsroa.xyz</title>
         <meta name="description" />
@@ -17,6 +41,18 @@ export default function Home() {
 				One man army
         </h1>
 
+		<div className='container h-[800px]' >
+				<Canvas>
+				<OrbitControls/>
+				<directionalLight intensity={0.4} position={new Vector3(8,30,-100)} lookAt={() => origin} />
+				<directionalLight intensity={0.2} position={new Vector3(8,20,-100)} lookAt={() => origin} />
+				<directionalLight intensity={0.8} color={new Color("00E1FF")} position={new Vector3(0,0,120)} lookAt={() => origin} />
+				<ambientLight intensity={0.2}/>
+					<Suspense fallback={<p>Loading</p>}>
+						<Model scale={0.3} rotation={new Euler(0,105+ scrollY)} />
+					</Suspense> 
+				</Canvas> 
+		</div>
         <p className="my-10">
           Get started by editing{' '}
           <code className="">pages/index.tsx</code>
